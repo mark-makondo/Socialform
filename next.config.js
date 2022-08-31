e58244bae570bd800447cb3659ba3e4a04a79da2
Mark Makondo <markalbert.makondo@gmail.com>
2022-08-31 21:21:07 +0300

    Updated NextJs to latest and added svg config
    - added svgr webpack config for svg importing.
    - we are now using the latest nextjs 12.2.
    
    Signed-off by Mark Makondo <markalbert.makondo@gmail.com>

--- next.config.js
+++ next.config.js
@@ -2,6 +2,26 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      loader: '@svgr/webpack',
+      options: {
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'preset-default',
+              params: {
+                overrides: { removeViewBox: false },
+              },
+            },
+          ],
+        },
+      },
+    });
+
+    return config;
+  }
 }
 
 /**
