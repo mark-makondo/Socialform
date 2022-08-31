e50fc5c080eead92b454a18af6abe7c4a1e28074
Mark Makondo <markalbert.makondo@gmail.com>
2022-08-31 14:39:45 +0300

    Initialized css variables and properties and files
    - added all typography requirements.
    - added all the colors needed.
    - added all the assets needed.
    - setup layout for global declarations.
    
    Signed-off by Mark Makondo <markalbert.makondo@gmail.com>

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,18 @@
+import Head from 'next/head';
 import layoutStyle from '../styles/components/layout.module.scss';
 import { CommonReusableProps } from '../types/common.type';
 
 const Layout: React.FC<CommonReusableProps> = ({ children }) => {
-    return <div className={layoutStyle.base}>{children}</div>;
+    return (
+        <div className={layoutStyle.base}>
+            <Head>
+                <title>Socialform</title>
+                <meta name="description" content="A social media platform with a twist." />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            {children}
+        </div>
+    );
 };
 
 export default Layout;
